ac2a9dfee0cf054dfe19e0b87c6073b0ea48821d
Caarribada <2021268@student.cct.ie>
2022-12-18 23:56:15 +0000

    CA-EquationInterface

---
+++ src/main/java/interfaces/EquaInterface.java
@@ -0,0 +1,7 @@
+package interfaces;
+
+public interface EquaInterface {
+    public String removeWhiteSpaces(String expression);
+    public double[] getA(String expression);
+    public double getb(String expression);
+}
